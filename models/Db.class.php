aa26a39a5bf8ec358cadab5dc76f380a0a484cc1
magicien-byte <71492373+magicien-byte@users.noreply.github.com>
2021-11-15 11:26:49 +0100

    Update Db.class.php

--- models/Db.class.php
+++ models/Db.class.php
@@ -7,7 +7,8 @@ class Db
     private function __construct()
     {
         try {
-            $this->_db = new PDO('mysql:host=localhost;dbname=bdbn;charset=utf8', 'root', '');
+            $this->_db = new PDO('mysql:host=ec2-34-253-116-145.eu-west-1.compute.amazonaws.com
+;dbname=dccdlmgve93f9b;charset=utf8', 'gacgbbbkakgozk', 'c3b20267500b4680932b14af5b0c70e8a089dffe92a6bd49acd814752b6ec942');
             $this->_db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         } 
 		catch (PDOException $e) {
